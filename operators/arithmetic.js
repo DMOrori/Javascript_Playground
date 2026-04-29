72d632d13b83fc3b3e23c0bed33b21b2526fa9cc
derrick-maebar <derrickorori@gmail.com>
2026-04-29 12:26:59 +0300

    added the folder operators and the file arithmetic operators

---
+++ operators/arithmetic.js
@@ -0,0 +1,15 @@
+var num1 = 10
+var num2 = 2
+var res = 0
+res = num1 + num2
+console.log("sum: " + res);
+res = num1 - num2
+console.log("difference: " + res);
+res = num1 * num2
+console.log("product: " + res);
+res = num1 % num2
+console.log("remainder: " + res);
+num1++
+console.log("num1: " + num1);
+num2--
+console.log("num2: " + num2);
