b8fd288887d556738e7d7e54203bc79fcf86b77c
derrick-maebar <derrickorori@gmail.com>
2026-04-29 12:42:36 +0300

    updated and added the logical operators file unto operators folder

---
+++ operators/relational.js
@@ -0,0 +1,16 @@
+var num1 = 5
+var num2 = 9
+console.log("value of num1: " + num1);
+console.log("value of num2: " + num2);
+var res = num1 > num2
+console.log("num1 greater than num2: " + res);
+res = num1 < num2
+console.log("num1 less than num2: " + res);
+res = num1 >= num2
+console.log("num1 greater than or equal to num2: " + res);
+res = num1 <= num2
+console.log("num1 less than or equal to num2: " + res);
+res = num1 == num2
+console.log("num1 equal to num2: " + res);
+res = num1 != num2
+console.log("num1 not equal to num2: " + res);
